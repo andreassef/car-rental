3526cfa04c9a257b1bc76a7a5dca065942ec6257
andreassef <andreassef02@gmail.com>
2021-03-20 23:02:37 -0400

    [NEW] - novo modulo de services para categorias

---
+++ src/service/CreateCategoryService.ts
@@ -0,0 +1,18 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateCategoryService {
+
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+
+    execute({name, description}: IRequest): void {
+        this.categoriesRepository.create({name, description});
+    }
+}
+
+export { CreateCategoryService };
