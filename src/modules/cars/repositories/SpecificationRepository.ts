40e43d359c0ef722ac679556d1f6fec94276f1bb
andreassef <andreassef02@gmail.com>
2021-03-21 21:15:56 -0400

    [NEW] - Novo repositorio para especificacao

---
+++ src/modules/cars/repositories/SpecificationRepository.ts
@@ -0,0 +1,30 @@
+import { Specification } from "../model/Specification";
+import { ICreateSpecificationDTO, ISpecificationRepository } from "./ISpecificationRepository";
+
+class SpecificationRepository implements ISpecificationRepository {
+    private specification: Specification[];
+    
+    constructor() {
+        this.specification = [];
+    }
+
+    create({ description, name }: ICreateSpecificationDTO): void {
+        const specification = new Specification();
+
+        Object.assign(specification, {
+            name,
+            description,
+            created_at: new Date(),
+        });
+
+        this.specification.push(specification);
+    }
+
+    findByName(name: string): Specification {
+        const specification = this.specification.find((spe) => spe.name === name);
+        return specification;
+    }
+
+}
+
+export { SpecificationRepository };
