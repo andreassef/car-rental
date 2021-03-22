28eccc4c74f08c9fcf285473eb1d390b7be4811d
andreassef <andreassef02@gmail.com>
2021-03-21 21:12:01 -0400

    [UPDATE] - Refatorando rota de categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,23 @@
 import { request, response, Router } from "express";
 import {v4 as uuidV4} from 'uuid';
-import { Category } from "../model/Category";
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
-import { CreateCategoryService } from "../service/CreateCategoryService";
+import { Category } from "../modules/cars/model/Category";
+import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { CreateCategoryUseCase } from "../modules/useCases/createCategory/CreateCategoryUseCase";
+import { createCategoryController, categoriesRepository } from "../modules/useCases/createCategory/index"
+import { listCategoriesController } from "../modules/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
-const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+// const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-    createCategoryService.execute({name, description})
-    return response.status(201).send();
+    return createCategoryController.handle(request, response);
 });
 
+// categoriesRoutes.post("/", createCategoryController.handle);
+
 categoriesRoutes.get("/", (request, response) => {
-    const list = categoriesRepository.list();
-    return response.status(200).json(list);
+    return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
