8a6eb403499809c7ac7a874c35559218cd57062a
andreassef <andreassef02@gmail.com>
2021-03-23 15:15:56 -0400

    [ADD] - Implementacao do swagger para documentação da api

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { router } from './routes';
-import {categoriesRoutes} from './routes/categories.routes';
-import { specificationRoutes } from './routes/specifications.routes';
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 app.use(express.json());
+
+import swaggerFile from "./swagger.json";
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
