94bd997db7ce74373f8ca492011d17ff03aa963b
andreassef <andreassef02@gmail.com>
2021-04-05 01:47:13 -0400

    [UPDATE] - Refatoracao na rota para criacao de nova categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 
-import createCategoryController from "../modules/cars/useCases/createCategory";
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
@@ -12,11 +12,9 @@ const upload = multer({
   dest: "./tmp",
 });
 
-// const categoriesRepository = new CategoriesRepository();
+const createCategoryController = new CreateCategoryController();
 
-categoriesRoutes.post("/", (request, response) => {
-  return createCategoryController().handle(request, response);
-});
+categoriesRoutes.post("/", createCategoryController.handle);
 
 // categoriesRoutes.post("/", createCategoryController.handle);
 
