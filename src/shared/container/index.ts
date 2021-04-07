a380eb3d2c5b26bdde08880725882317e9a0bc89
andreassef <andreassef02@gmail.com>
2021-04-06 21:35:29 -0400

    [ADD] - Novo container para specifications

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,16 @@ import { container } from "tsyringe";
 
 import { CategoriesRepository } from "../../modules/cars/repositories/CategoriesRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/implementations/ICategoriesRepository";
+import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationRepository";
+import { SpecificationRepository } from "../../modules/cars/repositories/SpecificationRepository";
 
 // ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository
+);
+
+container.registerSingleton<ISpecificationRepository>(
+  "SpecificationsRepository",
+  SpecificationRepository
 );
