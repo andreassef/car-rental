60cd5ef4b8c851be4ba301c79efe6b78326fd2bd
andreassef <andreassef02@gmail.com>
2021-04-13 17:55:18 -0400

    [NEW] - Middleware para verificar se o usuário está autenticado

---
+++ src/middlewares/ensureAuthentication.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error("Token missing");
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "61111a408ca2466345550d4d84765275"
+    ) as IPayload;
+    console.log(user_id);
+
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists!");
+    }
+
+    next();
+  } catch (error) {
+    throw new Error("Invalid token!");
+  }
+}
