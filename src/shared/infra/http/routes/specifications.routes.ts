ab1218b26b93f6a11054d6d0f1693f12e1056240
andreassef <andreassef02@gmail.com>
2021-04-28 22:51:46 -0400

    [ADD] - Adicionando middleware nas rotas

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 
 import { CreateSpecificationController } from "../../../../modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthentication";
 
 const specificationRoutes = Router();
 const createSpecificationController = new CreateSpecificationController();
 
-specificationRoutes.use(ensureAuthenticated);
-specificationRoutes.post("/", createSpecificationController.handle);
+// specificationRoutes.use(ensureAuthenticated);
+specificationRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationController.handle
+);
 
 export { specificationRoutes };
