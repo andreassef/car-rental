a1d4a52619bff8683447402fea9a9f6b4546b9f1
andreassef <andreassef02@gmail.com>
2021-04-30 23:58:00 -0400

    [ADD] - Adicao de novo metodo

--- src/modules/cars/repositories/ISpecificationRepository.ts
+++ src/modules/cars/repositories/ISpecificationRepository.ts
@@ -8,6 +8,7 @@ interface ICreateSpecificationDTO {
 interface ISpecificationRepository {
   create({ description, name }: ICreateSpecificationDTO): Promise<void>;
   findByName(name: string): Promise<Specification>;
+  findByIds(ids: string[]): Promise<Specification[]>;
 }
 
 export { ISpecificationRepository, ICreateSpecificationDTO };
