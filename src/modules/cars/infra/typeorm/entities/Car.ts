51bfacadf03a9fb14ae72b639d28b198ab461e1b
andreassef <andreassef02@gmail.com>
2021-05-01 03:19:43 -0400

    [UPDATE] - Ajuste no nome da tabela e na coluna

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -48,9 +48,9 @@ class Car {
 
   @ManyToMany(() => Specification)
   @JoinTable({
-    name: "specifications_car",
+    name: "specifications_cars",
     joinColumns: [{ name: "car_id" }],
-    inverseJoinColumns: [{ name: "specification_id " }],
+    inverseJoinColumns: [{ name: "specification_id" }],
   })
   specifications: Specification[];
 
