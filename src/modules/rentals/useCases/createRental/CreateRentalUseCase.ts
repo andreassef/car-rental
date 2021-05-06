e0bec202ad55c214532f550fc950d076a4434cc3
andreassef <andreassef02@gmail.com>
2021-05-05 23:43:05 -0400

    [UPDATE] - Implementando regra de negocio, tempo minimo para alugar um carro de 24h

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,8 +1,12 @@
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/infra/typeorm/repositories/IRentalsRepository";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
+dayjs.extend(utc);
 interface IRequest {
   user_id: string;
   car_id: string;
@@ -10,7 +14,10 @@ interface IRequest {
 }
 
 class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository) {}
+  constructor(
+    private rentalsRepository: IRentalsRepository,
+    private dateProvider: IDateProvider
+  ) {}
 
   async execute({
     user_id,
@@ -31,6 +38,24 @@ class CreateRentalUseCase {
 
     if (rentalOpenToUser) {
       throw new AppError("There's a rental in progress for user!");
+    }
+
+    // const expectedReturnDateFormat = dayjs(expected_return_date)
+    //   .utc()
+    //   .local()
+    //   .format();
+
+    const dateNow = this.dateProvider.dateNow();
+
+    const compare = this.dateProvider.compareInHours(
+      dateNow,
+      expected_return_date
+    );
+
+    const minHourAvailable = 24;
+
+    if (compare < minHourAvailable) {
+      throw new AppError("The return date cannot be less than 24 hours!");
     }
 
     const rental = await this.rentalsRepository.create({
