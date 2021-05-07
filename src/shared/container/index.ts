78dbf9eab78629631469dcf2e8e0af6ec2080505
andreassef <andreassef02@gmail.com>
2021-05-07 00:01:08 -0400

    [ADD] - importacao de container de data

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,6 +3,8 @@ import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRep
 import { SpecificationRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationRepository";
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/RentalsRepository";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { container } from "tsyringe";
 
 import { UsersRepository } from "../../modules/accounts/infra/repositories/UsersRepository";
@@ -10,6 +12,8 @@ import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepo
 import { CategoriesRepository } from "../../modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationRepository";
+
+import "@shared/container/providers";
 
 // ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
@@ -32,4 +36,9 @@ container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
 container.registerSingleton<ICarsImagesRepository>(
   "CarsImagesRepository",
   CarsImagesRepository
+);
+
+container.registerSingleton<IRentalsRepository>(
+  "RentalsRepository",
+  RentalsRepository
 );
