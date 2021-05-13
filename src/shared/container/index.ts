ae7479616c11ff54720ffc52b95e98cb9e4f4361
andreassef <andreassef02@gmail.com>
2021-05-13 12:07:01 -0400

    [UPDATE] - Movimentacao de arquivos de uma pasta para outra

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -7,7 +7,7 @@ import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/R
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { container } from "tsyringe";
 
-import { UsersRepository } from "../../modules/accounts/infra/repositories/UsersRepository";
+import { UsersRepository } from "../../modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 import { CategoriesRepository } from "../../modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
