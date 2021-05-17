dec4def00e25895c16f417083dce800034b61037
andreassef <andreassef02@gmail.com>
2021-05-16 20:04:42 -0400

    [NEW] - Novo controlador para recuperacao de senha

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { SendForgotPasswordUseCase } from "./SendForgotPasswordUseCase";
+
+class SendForgotPasswordMailController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+    const sendForgotPasswordMailUseCase = container.resolve(
+      SendForgotPasswordUseCase
+    );
+
+    await sendForgotPasswordMailUseCase.execute(email);
+
+    return response.send();
+  }
+}
+
+export { SendForgotPasswordMailController };
