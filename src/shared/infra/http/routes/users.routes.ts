19bcb80da279b26fd860b4e6601a27297139bdf5
andreassef <andreassef02@gmail.com>
2021-05-23 17:46:46 -0400

    [UPDATE] - refatoracao na importacao do uploadConfig

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -8,7 +8,7 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthentication";
 
 const usersRoutes = Router();
 
-const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+const uploadAvatar = multer(uploadConfig);
 
 const createUsersController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
