0046d73c6e49008cc1d75eaf0b7bb07f85a6ffaa
andreassef <andreassef02@gmail.com>
2021-05-23 18:49:35 -0400

    [UPDATE] - Aumento na duracao de vida do token

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret_token: "61111a408ca2466345550d4d84765275",
-  expires_in_token: "15m",
+  expires_in_token: "60m",
   secret_refresh_token: "e44c1ddbe83532bf3aa8942d8265f69e",
   expires_in_refresh_token: "30d",
   expires_refresh_token_days: 30,
